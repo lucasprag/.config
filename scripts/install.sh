015b07c4872fcfb79d56d156ecf6e93d667ccc18
Lucas Oliveira <loslucassilva@gmail.com>
2016-12-22 18:06:51 -0200

    Add a sample ackrc with some basic configs

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,6 +6,10 @@
 mv ~/.zshrc ~/.zshrc.original
 ln -sv ~/.dotfiles/zshrc ~/.zshrc
 
+# Ack
+mv ~/.ackrc ~/.ackrc.original
+ln -sv ~/.dotfiles/ackrc ~/.ackrc
+
 # tmux
 mv ~/.tmux.conf ~/.tmux.conf.original
 ln -sv ~/.dotfiles/tmux.conf ~/.tmux.conf
