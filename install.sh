1c4bf3cc6f5c1427ce1fd700fff767a5502cdbec
Lucas Oliveira <just.lucasoliveira@gmail.com>
2018-02-15 11:32:33 -0200

    Enable tmux to be my backup until https://github.com/syl20bnr/spacemacs/issues/10344

--- install.sh
+++ install.sh
@@ -12,3 +12,10 @@ ln -sv ~/.dotfiles/ackrc ~/.ackrc
 mv ~/.spacemacs ~/.spacemacs.original
 ln -sv ~/.dotfiles/spacemacs ~/.spacemacs
 
+# tmux
+mv ~/.tmux.conf ~/.tmux.conf.original
+ln -sv ~/.dotfiles/tmux.conf ~/.tmux.conf
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# tmuxinator
+curl https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.zsh > ~/.dotfiles/tmuxinator/tmuxinator.zsh
