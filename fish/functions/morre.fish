d14e1607ca4d41cd5490c5de497fcdf87fb289b3
Lucas Arantes <lucas.ruby.dev@gmail.com>
2019-07-03 12:37:50 -0400

    Improve morre function and add snakecase fish shell function

--- fish/functions/morre.fish
+++ fish/functions/morre.fish
@@ -1,4 +1,7 @@
+# kill all processes matching the given name
 # @more https://www.youtube.com/watch?v=YrrR--HjtWA
 function morre # deabo
   ps aux | grep $argv | awk '{print $2}' | xargs sudo kill
+  echo 'Dead?'
+  ps aux | grep $argv
 end
---
+++ fish/functions/snakecase.fish
@@ -0,0 +1,3 @@
+function snakecase
+  ruby -e "require 'facets'; puts '$argv'.snakecase"
+end
