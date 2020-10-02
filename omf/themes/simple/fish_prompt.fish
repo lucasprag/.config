1a66f6833243bba0f790feb71164f6074f35dcae
Lucas Arantes <lucas.ruby.dev@gmail.com>
2020-10-02 19:45:04 -0400

    Change theme for tmuc and fish to match gruvbox

--- omf/themes/simple/fish_prompt.fish
+++ omf/themes/simple/fish_prompt.fish
@@ -11,13 +11,16 @@ function fish_prompt
 
   set -l cyan (set_color cyan)
   set -l yellow (set_color yellow)
+  set -l bryellow (set_color bryellow)
+  set -l brmagenta (set_color brmagenta)
   set -l red (set_color red)
   set -l blue (set_color blue)
   set -l brblue (set_color brblue)
   set -l green (set_color green)
+  set -l brgreen (set_color brgreen)
   set -l normal (set_color normal)
 
-  set -l cwd $brblue(basename (pwd | sed "s:^$HOME:~:"))
+  set -l cwd $bryellow(basename (pwd | sed "s:^$HOME:~:"))
 
   # Display [venvname] if in a virtualenv
   if set -q VIRTUAL_ENV
@@ -32,9 +35,9 @@ function fish_prompt
     set -l git_branch (_git_branch_name)
 
     if [ (_git_is_dirty) ]
-      set git_info $yellow $git_branch " *" $normal
+      set git_info $brmagenta $git_branch " *" $normal
     else
-      set git_info $cyan $git_branch $normal
+      set git_info $brgreen $git_branch $normal
     end
     echo -n -s ' · ' $git_info $normal
   end
